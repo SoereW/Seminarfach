da9ffba40df6cf4c7ba4ec99564ee507f959d8e9
SoereW <soeren.wolleschensky@outlook.de>
2024-03-18 21:08:23 +0100

    Durschnittshelligkeit hinzugefügt

--- supernewfabulousNEWProgram.py
+++ supernewfabulousNEWProgram.py
@@ -4,6 +4,10 @@ from PIL import Image
 
 
 def VideoBilder(Videopfad):
+    #vorherige Ergebnisse löschen
+    for filename in os.listdir('extrahierte_bilder'):
+        os.remove(os.path.join('extrahierte_bilder', filename))
+
     video_path = Videopfad
     output_folder = 'extrahierte_bilder'
     os.makedirs(output_folder, exist_ok=True)
@@ -27,22 +31,21 @@ def VideoBilder(Videopfad):
     cv2.destroyAllWindows()
 def Ordnerauslesen(Bildpfad):
     bilder_liste = glob.glob(os.path.join(Bildpfad, '*.jpg'))
-    print(f'Es wurden {str(len(bilder_liste))} Bilder in dem Ordner gefunden')
+    print(f'Es wurden {str(len(bilder_liste))} Bilder in dem Ordner gefunden' + " ---  geschätzte Zeit:" + str(len(bilder_liste)/57))
     Bilderzusammenfügen(bilder_liste, Image.open(bilder_liste[0]).size)
 
 def Bilderzusammenfügen(ListeBilder, imgsize):
     umgedrehte_imgsize = (imgsize[1], imgsize[0])
     durchschnittsfoto = np.zeros(umgedrehte_imgsize, dtype=np.float64)
-    i=0
     for Bild in ListeBilder:
         img = Image.open(Bild)
         # Bild in Graustufen konvertieren und zu einem Numpy-Array konvertieren
         bild_array = np.array(img.convert('L'), dtype=np.float64)
-        print(i)
-        i+=1
         durchschnittsfoto += bild_array
     durchschnittsfoto /=len(ListeBilder)
+    print("Durschnittshelligkeit vom Ausgangsbild beträgt:" + str(np.sum(durchschnittsfoto)/(imgsize[0]*imgsize[1])))
     durchschnittsfoto = durchschnittsfoto.astype(np.uint8)
+
     Image.fromarray(durchschnittsfoto.astype(np.uint8)).save("Ausgansbild.jpg")
 def analyse(im, faktor):
     #sw_bild = im.convert('L')
@@ -75,7 +78,8 @@ def analyse(im, faktor):
             # print(type(brightness_values[i][j]))
             kontrast_bild.putpixel((j, i), int(brightness_values[i][j]))  # https://www.youtube.com/watch?v=5QR-dG68eNE
     return kontrast_bild
-#Ordnerauslesen('extrahierte_bilder')
+VideoBilder(r"C:\Users\soere\OneDrive\Videos\SeminarfachProjektVideos\Aufnahmen28.2.24ExperimentPolarisation\Geschnitten\Video32\117.mp4")
+Ordnerauslesen('extrahierte_bilder')
 img = Image.open('Ausgansbild.jpg')
 neuesBild = analyse(img,5)
 neuesBild.save('Harald.jpg')
